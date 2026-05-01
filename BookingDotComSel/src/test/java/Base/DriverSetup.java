986f22b510a170a7b5691001b783510d5759fc91
Suvam Sadhu <sadhu1616@gmail.com>
2026-05-02 02:03:53 +0530

    headless revert

--- BookingDotComSel/src/test/java/Base/DriverSetup.java
+++ BookingDotComSel/src/test/java/Base/DriverSetup.java
@@ -12,63 +12,42 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverSetup {
-    public static WebDriver driver;
-    public static Properties prop;
-
-    public static WebDriver getDriver() {
-        try {
-            FileInputStream ip = new FileInputStream(
-                System.getProperty("user.dir") + "/src/test/resources/config.properties"
-            );
-
-            prop = new Properties();
-            prop.load(ip);
-
-            String browser = prop.getProperty("browser");
-
-            if (browser.equalsIgnoreCase("chrome")) {
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless=new");
-                options.addArguments("--disable-gpu");
-                options.addArguments("--window-size=1920,1080");
-
-                driver = new ChromeDriver(options);
-            } 
-            else if (browser.equalsIgnoreCase("firefox")) {
-                FirefoxOptions options = new FirefoxOptions();
-                options.addArguments("--headless");
-
-                driver = new FirefoxDriver(options);
-            } 
-            else if (browser.equalsIgnoreCase("edge")) {
-                EdgeOptions options = new EdgeOptions();
-                options.addArguments("--headless=new");
-                options.addArguments("--disable-gpu");
-                options.addArguments("--window-size=1920,1080");
-
-                driver = new EdgeDriver(options);
-            } 
-            else {
-                System.out.println("Browser not found!! Running in Chrome");
-
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("--headless=new");
-                options.addArguments("--disable-gpu");
-                options.addArguments("--window-size=1920,1080");
-
-                driver = new ChromeDriver(options);
-            }
-
-        } catch (Exception e) {
-            System.out.println("File not found or driver setup failed: " + e.getMessage());
-        }
-
-        return driver;
-    }
-
-    public static void driverClose() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+	public static WebDriver driver;
+	public static Properties prop;
+	
+	public static WebDriver getDriver() {
+		try {
+		FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+"/src/test/resources/config.properties");
+		prop = new Properties();
+		prop.load(ip);
+		String browser = prop.getProperty("browser");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920,1080");
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		}
+		else if(browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
+		else if(browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
+		else {
+			System.out.println("Browser not found!! Running in Chrome");
+			driver = new ChromeDriver();
+		}
+		}
+		catch(Exception e)
+		{
+			System.out.println("file not found!!");
+		}
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	public static void driverClose() {
+		driver.quit();
+	}
 }
